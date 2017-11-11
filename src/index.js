fa8997f7b80e3341d7e708d8e09da5af9f44885f
Andrew Piper <pipera@uw.edu>
2017-11-10 16:18:55 -0800

    added options, fixes

--- src/index.js
+++ src/index.js
@@ -5,14 +5,36 @@ var chart = new Main({
   data: {
     // dataset to put on the chart. minimum x,y
     dataset: {
+      x: ['1','2','3','4','5'],
+      y: [3,65,23,63,0],
+      //y: [3, 51, 5, 0, 0, 51, 1, 2, 3, 4, 5],
+      //x: [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 11]
+    },
+    // scaling maximums for xy coordinates.
+    //scales: {x: 10, y: 20},
+    title: "no zeroes",
+    size: {width: 500, height: 200, margin: 25},
+    selector: "bar-chart-id",
+    type: "bar",
+    options: {
+      show_zeroes: false
+    },
+  }
+})
+
+var chart2 = new Main({
+  target: document.querySelector("#main"),
+  data: {
+    // dataset to put on the chart. minimum x,y
+    dataset: {
       //x: ['1','2','3','4','5'],
-      x: [1, 2, 3, 4, 5],
-      y: [10, 9, 8,7,6]
+      y: [3, 51, 5, 0, 0, 51, 1, 2, 3, 4, 5],
+      x: [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
     },
     // scaling maximums for xy coordinates.
     //scales: {x: 10, y: 20},
     size: {width: 500, height: 200, margin: 25},
-    selector: "bar-chart-id",
+    selector: "bar-chart-id2",
     type: "bar"
   }
 })
